8f61aa63c7a66f82baf3ac48f34e5db58e58a713
Marcus Saad <mv.nsaad@gmail.com>
2026-03-12 08:57:07 -0300

    Cleanup
    Remove redundant html file, add missing health check endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from frontend.views import index
+from frontend.health import health
 
 urlpatterns = [
     path("",            index),
+    path("health/",     health),
     path("api/users/",  include("users.urls")),
     path("api/rooms/",  include("rooms.urls")),
-    path("sms/",        include("sms.urls")),
-]
+    path("sms/",        include("sms.urls")),]
